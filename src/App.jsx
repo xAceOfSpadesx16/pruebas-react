8bb3a899ae75f7bae0b1c80546a170a5eab3eebe
xAceOfSpadesx16 <75001965+xAceOfSpadesx16@users.noreply.github.com>
2023-07-27 20:08:47 -0300

    .

--- src/App.jsx
+++ src/App.jsx
@@ -1,41 +1,12 @@
-import DataTable from 'react-data-table-component';
 import { productos } from '../productos.json';
 import { Component } from 'react';
 
-const data = productos;
-
-const columns = [
-	{
-		name: ' Nombre',
-		selector: 'Nombre',
-		sortable: true,
-	},
-	{
-		name: 'Marca',
-		selector: 'Marca',
-	},
-	{
-		name: 'Categoria',
-		selector: 'Categoria',
-	},
-	{
-		name: 'Codigo de Barras',
-		selector: 'CodigoProducto',
-	},
-];
-
-class MiTabla extends Component {
-	render() {
-		return (
-			<DataTable
-				pagination
-				responsive
-				direction='ltr'
-				title='Productos en Datatables'
-				columns={columns}
-				data={data}></DataTable>
-		);
-	}
-}
+const MiTabla = () => {
+	const fetchData = () => {};
+	return fetchData().map(producto => {
+		console.log('caca');
+		console.log('caca2');
+	});
+};
 
 export default MiTabla;
